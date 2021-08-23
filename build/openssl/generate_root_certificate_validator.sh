2cf81f2963000ebb63993b4a791a96792de921fd
Sanjay Vasandani <sanjayvas@google.com>
2021-08-23 15:48:04 -0700

    Require DNS hostname to be specified for certificate generation macros.
    This hostname gets set in the Subject Alternative Name (SAN) X.509 v3 extension of the generated certificate. Prior to this change, the value was hard-coded to "localhost".
    
    This also moves the macros to the //build/openssl Bazel package to match the existing convention for build defs that call an external binary.

---
+++ build/openssl/generate_root_certificate_validator.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#
+# Validate that root certificates were generated correctly
+#
+# Copyright 2021 The Cross-Media Measurement Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+ source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+ source "$0.runfiles/$f" 2>/dev/null || \
+ source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+ source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+ { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+set -eEu -o pipefail
+
+readonly WORKSPACE='wfa_common_jvm'
+readonly TESTDATA_DIR="${WORKSPACE}/build/openssl"
+readonly CERT_PATH="${TESTDATA_DIR}/test_root.pem"
+readonly KEY_PATH="${TESTDATA_DIR}/test_root.key"
+readonly ORG='Some Organization'
+readonly COMMON_NAME='Some CA'
+readonly SUBJECT="subject=O = ${ORG}, CN = ${COMMON_NAME}"
+readonly HOSTNAME='ca.someorg.example.com'
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+main() {
+  local cert_file
+  cert_file="$(rlocation "${CERT_PATH}")"
+  local key_file
+  key_file="$(rlocation "${KEY_PATH}")"
+
+  # Check that certificate verifies itself when it is set as the CA.
+  openssl verify -x509_strict -verbose -CAfile "${cert_file}" -verify_hostname "${HOSTNAME}" "${cert_file}"
+
+  # Check that private key is proper EC key
+  openssl ec -check -noout -in "${key_file}"
+
+  # Check that public keys match
+  if [[ "$(openssl pkey -pubout -in "${key_file}")" != "$(openssl x509 -noout -pubkey -in "${cert_file}")" ]]; then
+    err 'Public keys do not match'
+    exit 1
+  fi
+
+  # Check the certificate details
+  local subject
+  subject="$(openssl x509 -in "${cert_file}" -noout -subject)"
+  if [[ "${subject}" != "${SUBJECT}" ]]; then
+    err "Not true that ${subject} is equal to ${SUBJECT}"
+    exit 1
+  fi
+}
+
+main "$@"
