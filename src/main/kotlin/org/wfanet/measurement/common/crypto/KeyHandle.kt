1e439e0b4650f494b07318b3aca2897b39826fb0
Sanjay Vasandani <sanjayvas@google.com>
2021-10-13 14:52:01 -0700

    Add key storage and cryptor interfaces with skeleton Tink implementations.
    This replaces some of the interfaces defined in #45.

---
+++ src/main/kotlin/org/wfanet/measurement/common/crypto/KeyHandle.kt
@@ -0,0 +1,34 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.common.crypto
+
+import com.google.protobuf.ByteString
+
+/** Handle to a cryptographic key. */
+interface KeyHandle {
+  val keyId: String
+}
+
+/** Handle to the public key of an encryption key pair. */
+interface PublicKeyHandle : KeyHandle {
+  fun hybridEncrypt(plaintext: ByteString, contextInfo: ByteString? = null): ByteString
+}
+
+/** Handle to the private key of an encryption key pair. */
+interface PrivateKeyHandle : KeyHandle {
+  val publicKey: PublicKeyHandle
+
+  fun hybridDecrypt(ciphertext: ByteString, contextInfo: ByteString? = null): ByteString
+}
