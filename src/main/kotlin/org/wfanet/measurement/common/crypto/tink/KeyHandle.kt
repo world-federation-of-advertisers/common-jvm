3b911d334701803b7de0ce975ee348a44e86beef
Sanjay Vasandani <sanjayvas@google.com>
2021-10-14 10:28:18 -0700

    Add key storage and cryptor interfaces with skeleton Tink implementations.
    This replaces some of the interfaces defined in #45.

---
+++ src/main/kotlin/org/wfanet/measurement/common/crypto/tink/KeyHandle.kt
@@ -0,0 +1,65 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.common.crypto.tink
+
+import com.google.crypto.tink.BinaryKeysetReader
+import com.google.crypto.tink.BinaryKeysetWriter
+import com.google.crypto.tink.KeysetHandle
+import com.google.protobuf.ByteString
+import org.wfanet.measurement.common.crypto.PrivateKeyHandle
+import org.wfanet.measurement.common.crypto.PublicKeyHandle
+
+class TinkPublicKeyHandle internal constructor(internal val keysetHandle: KeysetHandle) :
+  PublicKeyHandle {
+
+  init {
+    require(!keysetHandle.primaryKey().hasSecret())
+  }
+
+  constructor(serializedKeyset: ByteString) : this(parseKeyset(serializedKeyset))
+
+  fun toByteString(): ByteString {
+    return ByteString.newOutput().use {
+      keysetHandle.writeNoSecret(BinaryKeysetWriter.withOutputStream(it))
+      it.toByteString()
+    }
+  }
+
+  override fun hybridEncrypt(plaintext: ByteString, contextInfo: ByteString?): ByteString {
+    TODO("Not yet implemented")
+  }
+
+  companion object {
+    private fun parseKeyset(serialized: ByteString): KeysetHandle {
+      return serialized.newInput().use {
+        KeysetHandle.readNoSecret(BinaryKeysetReader.withInputStream(it))
+      }
+    }
+  }
+}
+
+class TinkPrivateKeyHandle internal constructor(internal val keysetHandle: KeysetHandle) :
+  PrivateKeyHandle {
+
+  init {
+    require(keysetHandle.primaryKey().hasSecret())
+  }
+
+  override val publicKey = TinkPublicKeyHandle(keysetHandle.publicKeysetHandle)
+
+  override fun hybridDecrypt(ciphertext: ByteString, contextInfo: ByteString?): ByteString {
+    TODO("Not yet implemented")
+  }
+}
