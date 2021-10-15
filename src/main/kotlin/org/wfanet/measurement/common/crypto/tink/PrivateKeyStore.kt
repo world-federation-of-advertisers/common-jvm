43cffbd9ab0ceaf6454f5c00035aef1ec3ac7fcc
Kyle Johnson <kej@fb.com>
2021-10-15 15:08:38 -0500

    Implement all TODOs for Tink skeletons (#49)
    * Implement all TODOs for Tink skeletons
    
    - Fix review comments
    - Rebase to latest changes upstream

--- src/main/kotlin/org/wfanet/measurement/common/crypto/tink/PrivateKeyStore.kt
+++ src/main/kotlin/org/wfanet/measurement/common/crypto/tink/PrivateKeyStore.kt
@@ -15,17 +15,31 @@
 package org.wfanet.measurement.common.crypto.tink
 
 import com.google.crypto.tink.Aead
+import com.google.crypto.tink.BinaryKeysetReader
+import com.google.crypto.tink.BinaryKeysetWriter
+import com.google.crypto.tink.KeysetHandle
+import com.google.protobuf.ByteString
 import org.wfanet.measurement.common.crypto.PrivateKeyStore as CryptoPrivateKeyStore
+import org.wfanet.measurement.common.flatten
 import org.wfanet.measurement.storage.Store
+import org.wfanet.measurement.storage.read
 
 internal class PrivateKeyStore(private val store: Store<String>, private val aead: Aead) :
   CryptoPrivateKeyStore<TinkPrivateKeyHandle> {
 
   override suspend fun read(keyId: String): TinkPrivateKeyHandle? {
-    TODO("Not yet implemented")
+    val privateKeyBlob = store.get(keyId) ?: return null
+    return privateKeyBlob.read().flatten().newInput().use {
+      TinkPrivateKeyHandle(KeysetHandle.read(BinaryKeysetReader.withInputStream(it), aead))
+    }
   }
 
   override suspend fun write(privateKey: TinkPrivateKeyHandle): String {
-    TODO("Not yet implemented")
+    return ByteString.newOutput().use {
+      privateKey.keysetHandle.write(BinaryKeysetWriter.withOutputStream(it), aead)
+      val privateKeyBlob =
+        store.write(privateKey.keysetHandle.keysetInfo.primaryKeyId.toString(), it.toByteString())
+      privateKeyBlob.blobKey
+    }
   }
 }
