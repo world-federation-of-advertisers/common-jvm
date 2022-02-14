8910f92be3b14f7523fcc4f70bf314e9d4df661d
renjiez <renjiez@google.com>
2022-02-14 22:50:33 +0000

    Use assertFailWith() to test failGrpc

--- src/test/kotlin/org/wfanet/measurement/common/grpc/PreconditionsTest.kt
+++ src/test/kotlin/org/wfanet/measurement/common/grpc/PreconditionsTest.kt
@@ -18,31 +18,34 @@ import com.google.common.truth.Truth.assertThat
 import io.grpc.Status
 import io.grpc.StatusException
 import io.grpc.StatusRuntimeException
+import kotlin.test.assertFailsWith
 import org.junit.Test
 
 class PreconditionsTest {
   @Test
   fun `failGrpc without cause`() {
-    try {
-      failGrpc(Status.FAILED_PRECONDITION) { "No cause" }
-    } catch (e: StatusRuntimeException) {
-      assertThat(e.status.code).isEqualTo(Status.Code.FAILED_PRECONDITION)
-      assertThat(e.status.cause).isEqualTo(null)
-      assertThat(e.status.description).isEqualTo("No cause")
-      assertThat(e.status.cause).isEqualTo(null)
-    }
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        failGrpc(Status.FAILED_PRECONDITION) { "No cause" }
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.FAILED_PRECONDITION)
+    assertThat(exception.status.cause).isEqualTo(null)
+    assertThat(exception.status.description).isEqualTo("No cause")
+    assertThat(exception.status.cause).isEqualTo(null)
   }
 
   @Test
   fun `failGrpc with cause`() {
-    val cause = StatusException(Status.INTERNAL)
-    try {
-      failGrpc(Status.FAILED_PRECONDITION, cause) { "Cause of internal error" }
-    } catch (e: StatusRuntimeException) {
-      assertThat(e.status.code).isEqualTo(Status.Code.FAILED_PRECONDITION)
-      assertThat(e.status.cause.toString()).isEqualTo(StatusException(Status.INTERNAL).toString())
-      assertThat(e.status.description).isEqualTo("Cause of internal error")
-      assertThat(e.cause.toString()).isEqualTo(StatusException(Status.INTERNAL).toString())
-    }
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        failGrpc(Status.FAILED_PRECONDITION, StatusException(Status.INTERNAL)) {
+          "Cause of internal error"
+        }
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.FAILED_PRECONDITION)
+    assertThat(exception.status.cause.toString())
+      .isEqualTo(StatusException(Status.INTERNAL).toString())
+    assertThat(exception.status.description).isEqualTo("Cause of internal error")
+    assertThat(exception.cause.toString()).isEqualTo(StatusException(Status.INTERNAL).toString())
   }
 }
