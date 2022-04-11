498260315f94c3d4d0740389e5ad0aac7d8957e8
Steven Ware Jones <stevenwjones@fb.com>
2022-04-11 17:08:16 -0400

    fix linting

--- src/main/kotlin/org/wfanet/measurement/common/crypto/SecurityProvider.kt
+++ src/main/kotlin/org/wfanet/measurement/common/crypto/SecurityProvider.kt
@@ -115,8 +115,8 @@ fun KeySpec.toPrivateKey(algorithm: String): PrivateKey {
  * Extracts a key identifier from an extension.
  *
  * Assumes that the first instance of a byte of [KEY_IDENTIFIER_LENGTH] is the length octet and that
- * the key identifier of length [KEY_IDENTIFIER_LENGTH] immediately follows.
- * TODO: Implement a more secure method of extracting key identifiers.
+ * the key identifier of length [KEY_IDENTIFIER_LENGTH] immediately follows. TODO: Implement a more
+ * secure method of extracting key identifiers.
  */
 fun X509Certificate.extractExtensionKeyIdentifier(extensionOid: String): ByteString? {
   val extension: ByteArray = getExtensionValue(extensionOid) ?: return null
