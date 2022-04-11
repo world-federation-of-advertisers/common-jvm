91b8802bb7f6a1db6cedcc3d623771c12dc136bf
Steven Ware Jones <stevenwjones@fb.com>
2022-04-11 17:16:17 -0400

    Parse a more diverse set of key identifiers from ASN1 (#101)
    * support reading key identifiers from other asn1 formats

--- src/main/kotlin/org/wfanet/measurement/common/crypto/SecurityProvider.kt
+++ src/main/kotlin/org/wfanet/measurement/common/crypto/SecurityProvider.kt
@@ -35,6 +35,9 @@ private const val CERTIFICATE_TYPE = "X.509"
 private const val SUBJECT_KEY_IDENTIFIER_OID = "2.5.29.14"
 private const val AUTHORITY_KEY_IDENTIFIER_OID = "2.5.29.35"
 
+/** All Ski and Aki must have length 20. */
+private const val KEY_IDENTIFIER_LENGTH: Int = 20
+
 /**
  * Known ASN.1 DER tags.
  *
@@ -47,7 +50,7 @@ private enum class Asn1Tag(val byte: Byte) {
   /** Constructed tag for SEQUENCE type. */
   SEQUENCE(0x30.toByte()),
   /** Context-specific tag for keyIdentifier within AuthorityKeyIdentifier extension. */
-  KEY_IDENTIFIER(0x80.toByte())
+  KEY_IDENTIFIER(0x80.toByte()),
 }
 
 private val conscryptProvider: Provider =
@@ -109,18 +112,35 @@ fun KeySpec.toPrivateKey(algorithm: String): PrivateKey {
 }
 
 /**
+ * Extracts a key identifier from an extension.
+ *
+ * Assumes that the first instance of a byte of [KEY_IDENTIFIER_LENGTH] is the length octet and that
+ * the key identifier of length [KEY_IDENTIFIER_LENGTH] immediately follows. TODO: Implement a more
+ * secure method of extracting key identifiers.
+ */
+fun X509Certificate.extractExtensionKeyIdentifier(extensionOid: String): ByteString? {
+  val extension: ByteArray = getExtensionValue(extensionOid) ?: return null
+  val index = extension.indexOf(KEY_IDENTIFIER_LENGTH.toByte())
+  if (index == -1) {
+    return null
+  }
+  if (index + 1 + KEY_IDENTIFIER_LENGTH > extension.size) {
+    return null
+  }
+  val bytes = ByteString.copyFrom(extension, index + 1, KEY_IDENTIFIER_LENGTH)
+  if (bytes.size() != KEY_IDENTIFIER_LENGTH) {
+    return null
+  }
+  return bytes
+}
+
+/**
  * The keyIdentifier from the SubjectKeyIdentifier (AKI) X.509 extension, or `null` if it cannot be
  * found.
  */
 val X509Certificate.subjectKeyIdentifier: ByteString?
   get() {
-    val extension: ByteArray = getExtensionValue(SUBJECT_KEY_IDENTIFIER_OID) ?: return null
-    if (extension.size < 4 || extension[2] != Asn1Tag.OCTET_STRING.byte) {
-      return null
-    }
-
-    val length = extension[3].toInt() // Assuming short form, where length <= 127 bytes.
-    return ByteString.copyFrom(extension, 4, length)
+    return extractExtensionKeyIdentifier(SUBJECT_KEY_IDENTIFIER_OID)
   }
 
 /**
@@ -129,16 +149,7 @@ val X509Certificate.subjectKeyIdentifier: ByteString?
  */
 val X509Certificate.authorityKeyIdentifier: ByteString?
   get() {
-    val extension: ByteArray = getExtensionValue(AUTHORITY_KEY_IDENTIFIER_OID) ?: return null
-    if (extension.size < 6 ||
-        extension[2] != Asn1Tag.SEQUENCE.byte ||
-        extension[4] != Asn1Tag.KEY_IDENTIFIER.byte
-    ) {
-      return null
-    }
-
-    val length = extension[5].toInt() // Assuming short form, where length <= 127 bytes.
-    return ByteString.copyFrom(extension, 6, length)
+    return extractExtensionKeyIdentifier(AUTHORITY_KEY_IDENTIFIER_OID)
   }
 
 /** Generates a new [KeyPair]. */
