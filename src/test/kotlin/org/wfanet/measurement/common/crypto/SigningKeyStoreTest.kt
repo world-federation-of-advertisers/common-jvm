85aaedafad8879682d7967dfdd1c99fd6b8617d5
Sanjay Vasandani <sanjayvas@google.com>
2022-05-31 10:51:42 -0700

    Take in StorageClient instead of Store in SigningKeyStore constructor.
    SigningKeyStore initializes its private Store implementation.

--- src/test/kotlin/org/wfanet/measurement/common/crypto/SigningKeyStoreTest.kt
+++ src/test/kotlin/org/wfanet/measurement/common/crypto/SigningKeyStoreTest.kt
@@ -18,51 +18,44 @@ import com.google.common.truth.Truth.assertThat
 import java.security.cert.X509Certificate
 import kotlin.test.assertNotNull
 import kotlinx.coroutines.runBlocking
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.wfanet.measurement.common.crypto.testing.FIXED_SERVER_CERT_PEM_FILE
 import org.wfanet.measurement.common.crypto.testing.FIXED_SERVER_KEY_FILE
 import org.wfanet.measurement.common.flatten
-import org.wfanet.measurement.storage.Store
-import org.wfanet.measurement.storage.filesystem.FileSystemStorageClient
+import org.wfanet.measurement.storage.testing.InMemoryStorageClient
 
 @RunWith(JUnit4::class)
 class SigningKeyStoreTest {
-  @get:Rule val tempDir = TemporaryFolder()
-
-  private lateinit var store: Store<SigningKeyStore.Context>
-  private lateinit var signingKeyStore: SigningKeyStore
-
-  @Before
-  fun initSigningKeyStore() {
-    val storageClient = FileSystemStorageClient(tempDir.root)
-    store =
-      object : Store<SigningKeyStore.Context>(storageClient) {
-        override val blobKeyPrefix: String = "sigKeys"
-
-        override fun deriveBlobKey(context: SigningKeyStore.Context): String = context.blobKey
-      }
-    signingKeyStore = SigningKeyStore(store)
-  }
+  private val storageClient = InMemoryStorageClient()
+  private val signingKeyStore = SigningKeyStore(storageClient)
 
   @Test
-  fun `write writes PEM file to store`() = runBlocking {
-    val keyId: String = SigningKeyHandle(certificate, privateKey).write(signingKeyStore)
+  fun `write writes PEM file to storage`() = runBlocking {
+    SigningKeyHandle(certificate, privateKey).write(signingKeyStore)
 
-    val blob = assertNotNull(store.get(keyId))
+    val blob = storageClient.contents.values.single()
     assertThat(blob.read().flatten().toStringUtf8()).isEqualTo(CONCATENATED_PEM)
   }
 
   @Test
-  fun `read reads signing key from store`() = runBlocking {
+  fun `read by blob context reads signing key from store`() = runBlocking {
     val privateKey = SigningKeyHandle(certificate, privateKey)
-    val keyId: String = privateKey.write(signingKeyStore)
+    privateKey.write(signingKeyStore)
 
-    val readKey: SigningKeyHandle = assertNotNull(signingKeyStore.read(keyId))
+    val readKey: SigningKeyHandle =
+      assertNotNull(signingKeyStore.read(SigningKeyStore.Context.fromCertificate(certificate)))
+
+    assertThat(readKey).isEqualTo(privateKey)
+  }
+
+  @Test
+  fun `read by blob key reads signing key from store`() = runBlocking {
+    val privateKey = SigningKeyHandle(certificate, privateKey)
+    val blobKey: String = privateKey.write(signingKeyStore)
+
+    val readKey: SigningKeyHandle = assertNotNull(signingKeyStore.read(blobKey))
 
     assertThat(readKey).isEqualTo(privateKey)
   }
