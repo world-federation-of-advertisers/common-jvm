765fccaadb0313b13a77f64306a90e405a9745c0
Sanjay Vasandani <sanjayvas@google.com>
2024-01-26 12:46:45 -0800

    Migrate to Bzlmod.
    This updates Bazel to 6.4.0 and Tink to 1.8.0.

--- src/main/kotlin/org/wfanet/measurement/common/Runfiles.kt
+++ src/main/kotlin/org/wfanet/measurement/common/Runfiles.kt
@@ -18,7 +18,17 @@ import com.google.devtools.build.runfiles.Runfiles
 import java.nio.file.Path
 import java.nio.file.Paths
 
-private val runfiles: Runfiles by lazy { Runfiles.create() }
+/**
+ * Returns the runtime [Path] for the given runfiles-root-relative [Path], or null if it cannot be
+ * found.
+ *
+ * Note that this may return a non-null value even if the path doesn't exist.
+ *
+ * @param runfilesRelativePath path relative to the Bazel runfiles root
+ */
+fun Runfiles.getRuntimePath(runfilesRelativePath: Path): Path? {
+  return rlocation(runfilesRelativePath.normalize().toString())?.let { Paths.get(it) }
+}
 
 /**
  * Returns the runtime [Path] for the given runfiles-root-relative [Path], or null if it cannot be
@@ -28,8 +38,12 @@ private val runfiles: Runfiles by lazy { Runfiles.create() }
  *
  * @param runfilesRelativePath path relative to the Bazel runfiles root
  */
+@Suppress("DeprecatedCallableAddReplaceWith")
+@Deprecated(
+  "May not work correctly with Bzlmod. Use Runfiles.getRuntimePath with Runfiles.preload()"
+)
 fun getRuntimePath(runfilesRelativePath: Path): Path? {
-  return runfiles.rlocation(runfilesRelativePath.normalize().toString())?.let { Paths.get(it) }
+  return Runfiles.create().getRuntimePath(runfilesRelativePath)
 }
 
 /**
