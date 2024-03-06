d46c37388f3c029ec86c101666259021455daa17
renjiez <renjiez@google.com>
2024-03-06 20:42:01 +0000

    Update TinkKeyId into an unique id as Long.

--- src/main/kotlin/org/wfanet/measurement/common/crypto/tink/TinkKeyId.kt
+++ src/main/kotlin/org/wfanet/measurement/common/crypto/tink/TinkKeyId.kt
@@ -16,11 +16,55 @@
 
 package org.wfanet.measurement.common.crypto.tink
 
+import com.google.crypto.tink.Aead
+import com.google.crypto.tink.KeysetWriter
+import com.google.crypto.tink.proto.EncryptedKeyset
+import com.google.crypto.tink.proto.Keyset
+import com.google.protobuf.ByteString
+import java.io.OutputStream
+import org.wfanet.measurement.common.crypto.Hashing
 import org.wfanet.measurement.common.crypto.PrivateKeyStore
+import org.wfanet.measurement.common.toLong
 
-data class TinkKeyId(private val primaryKeyId: Int) : PrivateKeyStore.KeyId {
-  constructor(publicKey: TinkPublicKeyHandle) : this(publicKey.keysetHandle.keysetInfo.primaryKeyId)
+data class TinkKeyId(private val primaryKeyId: Long) : PrivateKeyStore.KeyId {
+  constructor(publicKey: TinkPublicKeyHandle) : this(getTinkKeyId(publicKey))
 
   override val blobKey: String
     get() = primaryKeyId.toString()
+}
+
+private fun getTinkKeyId(publicKey: TinkPublicKeyHandle): Long {
+  val publicKeyData =
+    ByteString.newOutput().use {
+      publicKey.keysetHandle.write(CleartextKeysetWriter(it), CleartextAead())
+      it.toByteString()
+    }
+  return Hashing.hashSha256(publicKeyData).substring(0, 8).toLong()
+}
+
+internal class CleartextKeysetWriter(private val stream: OutputStream) : KeysetWriter {
+  override fun write(keyset: Keyset?) {
+    requireNotNull(keyset) { "Keyset cannot be null." }
+    val primaryKey =
+      keyset.keyList.find { it.keyId == keyset.primaryKeyId }
+        ?: error("Primary key cannot be found.")
+    stream.use { it.write(primaryKey.keyData.value.toByteArray()) }
+  }
+
+  override fun write(keyset: EncryptedKeyset?) {
+    requireNotNull(keyset) { "Keyset cannot be null." }
+    stream.use { it.write(keyset.encryptedKeyset.toByteArray()) }
+  }
+}
+
+internal class CleartextAead : Aead {
+  override fun encrypt(plaintext: ByteArray?, associatedData: ByteArray?): ByteArray {
+    requireNotNull(plaintext) { "plaintext cannot be null." }
+    return plaintext
+  }
+
+  override fun decrypt(ciphertext: ByteArray?, associatedData: ByteArray?): ByteArray {
+    requireNotNull(ciphertext) { "plaintext cannot be null." }
+    return ciphertext
+  }
 }
