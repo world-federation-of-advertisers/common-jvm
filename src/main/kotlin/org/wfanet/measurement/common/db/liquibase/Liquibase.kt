world-federation-of-advertisers/common-jvm

commit 2d63fc23a013a405ecc090dc0af996c97f76721d

Use CommandScope instead of deprecated Liquibase.update method. (#245)

src/main/kotlin/org/wfanet/measurement/common/db/liquibase/Liquibase.kt
@@ -20,24 +20,50 @@ import java.nio.file.Path
 import java.sql.Connection
 import java.util.logging.Level
 import java.util.logging.Logger
-import liquibase.Liquibase
 import liquibase.Scope
+import liquibase.UpdateSummaryOutputEnum
+import liquibase.changelog.ChangeLogParameters
+import liquibase.command.CommandScope
+import liquibase.command.core.UpdateCommandStep
+import liquibase.command.core.helpers.DatabaseChangelogCommandStep
+import liquibase.command.core.helpers.DbUrlConnectionArgumentsCommandStep
+import liquibase.command.core.helpers.ShowSummaryArgument
 import liquibase.database.DatabaseFactory
 import liquibase.database.jvm.JdbcConnection
 import liquibase.logging.core.JavaLogService
 import liquibase.resource.DirectoryResourceAccessor
 
 object Liquibase {
-  fun fromPath(connection: Connection, changelogPath: Path): Liquibase {
-    return Liquibase(
-      changelogPath.toString(),
-      DirectoryResourceAccessor(changelogPath.fileSystem.rootDirectories.first()),
+  fun update(connection: Connection, changelogPath: Path) {
+    val database =
       DatabaseFactory.getInstance().findCorrectDatabaseImplementation(JdbcConnection(connection))
-    )
+    val resourceAccessor =
+      DirectoryResourceAccessor(changelogPath.fileSystem.rootDirectories.first())
+    val scopeObjects =
+      mapOf(
+        Scope.Attr.database.name to database,
+        Scope.Attr.resourceAccessor.name to resourceAccessor,
+      )
+
+    Scope.child(scopeObjects) {
+      Scope.getCurrentScope().setLogLevel(Level.INFO)
+
+      CommandScope(*UpdateCommandStep.COMMAND_NAME)
+        .apply {
+          addArgumentValue(DbUrlConnectionArgumentsCommandStep.DATABASE_ARG, database)
+          addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, changelogPath.toString())
+          addArgumentValue(
+            DatabaseChangelogCommandStep.CHANGELOG_PARAMETERS,
+            ChangeLogParameters(database),
+          )
+          addArgumentValue(ShowSummaryArgument.SHOW_SUMMARY_OUTPUT, UpdateSummaryOutputEnum.LOG)
+        }
+        .execute()
+    }
   }
 }
 
-fun Scope.setLogLevel(level: Level) {
+private fun Scope.setLogLevel(level: Level) {
   val logService = get(Scope.Attr.logService, JavaLogService::class.java)
   if (logService.parent == null) {
     logService.parent = Logger.getLogger(this::class.java.name)

src/main/kotlin/org/wfanet/measurement/common/db/liquibase/tools/BaseUpdateSchema.kt
@@ -17,12 +17,8 @@
 package org.wfanet.measurement.common.db.liquibase.tools
 
 import java.sql.Connection
-import java.util.logging.Level
 import java.util.logging.Logger
-import liquibase.Contexts
-import liquibase.Scope
 import org.wfanet.measurement.common.db.liquibase.Liquibase
-import org.wfanet.measurement.common.db.liquibase.setLogLevel
 import org.wfanet.measurement.common.getJarResourcePath
 import picocli.CommandLine.Command
 import picocli.CommandLine.Option
@@ -42,12 +38,8 @@ abstract class BaseUpdateSchema : Runnable {
         "JAR resource $changelog not found"
       }
 
-    logger.info("Loading changelog from $changelogPath")
-    Liquibase.fromPath(connection, changelogPath).use { liquibase ->
-      logger.info("Updating...")
-      Scope.getCurrentScope().setLogLevel(Level.FINE)
-      liquibase.update(Contexts())
-    }
+    logger.info("Updating database with Liquibase changelog from $changelogPath")
+    Liquibase.update(connection, changelogPath)
   }
 
   companion object {

src/main/kotlin/org/wfanet/measurement/common/db/r2dbc/postgres/testing/PostgresDatabaseProvider.kt
@@ -20,16 +20,12 @@ import io.r2dbc.spi.ConnectionFactories
 import io.r2dbc.spi.ConnectionFactoryOptions
 import java.nio.file.Path
 import java.util.concurrent.atomic.AtomicInteger
-import java.util.logging.Level
 import kotlinx.coroutines.reactive.awaitFirst
-import liquibase.Contexts
-import liquibase.Scope
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 import org.testcontainers.containers.PostgreSQLContainer
 import org.wfanet.measurement.common.db.liquibase.Liquibase
-import org.wfanet.measurement.common.db.liquibase.setLogLevel
 import org.wfanet.measurement.common.db.r2dbc.postgres.PostgresDatabaseClient
 
 /** Provider of PostgreSQL databases. */
@@ -70,10 +66,7 @@ class PostgresDatabaseProviderRule(private val changelogPath: Path) :
 
     private fun KPostgresContainer.updateTemplateDatabase(changelogPath: Path) {
       withDatabaseName(TEMPLATE_DATABASE_NAME).createConnection("").use { connection ->
-        Liquibase.fromPath(connection, changelogPath).use { liquibase ->
-          Scope.getCurrentScope().setLogLevel(Level.FINE)
-          liquibase.update(Contexts())
-        }
+        Liquibase.update(connection, changelogPath)
       }
     }
 
@@ -91,7 +84,7 @@ class PostgresDatabaseProviderRule(private val changelogPath: Path) :
             .option(ConnectionFactoryOptions.HOST, host)
             .option(
               ConnectionFactoryOptions.PORT,
-              getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT)
+              getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
             )
             .option(ConnectionFactoryOptions.USER, username)
             .option(ConnectionFactoryOptions.PASSWORD, password)

src/main/kotlin/org/wfanet/measurement/gcloud/spanner/testing/SpannerEmulatorDatabaseRule.kt
@@ -19,15 +19,11 @@ import com.google.cloud.spanner.Spanner
 import com.google.cloud.spanner.connection.SpannerPool
 import java.nio.file.Path
 import java.sql.DriverManager
-import java.util.logging.Level
 import kotlinx.coroutines.runBlocking
-import liquibase.Contexts
-import liquibase.Scope
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 import org.wfanet.measurement.common.db.liquibase.Liquibase
-import org.wfanet.measurement.common.db.liquibase.setLogLevel
 import org.wfanet.measurement.gcloud.spanner.AsyncDatabaseClient
 import org.wfanet.measurement.gcloud.spanner.buildSpanner
 import org.wfanet.measurement.gcloud.spanner.getAsyncDatabaseClient
@@ -39,7 +35,7 @@ import org.wfanet.measurement.gcloud.spanner.getAsyncDatabaseClient
  */
 class SpannerEmulatorDatabaseRule(
   private val changelogPath: Path,
-  private val databaseName: String = "test-db"
+  private val databaseName: String = "test-db",
 ) : TestRule {
   lateinit var databaseClient: AsyncDatabaseClient
     private set
@@ -71,10 +67,7 @@ class SpannerEmulatorDatabaseRule(
     val connectionString =
       SpannerEmulator.buildJdbcConnectionString(emulatorHost, PROJECT, INSTANCE, databaseName)
     DriverManager.getConnection(connectionString).use { connection ->
-      Liquibase.fromPath(connection, changelogPath).use { liquibase ->
-        Scope.getCurrentScope().setLogLevel(Level.FINE)
-        liquibase.update(Contexts())
-      }
+      Liquibase.update(connection, changelogPath)
     }
 
     return buildSpanner(PROJECT, emulatorHost)

commit 6aab1f6a27a776836277e1355206948c2b6be0e1

Ensure Liquibase Database gets closed. (#248)
This addresses an issue introduced in #245 that could prevent Spanner JDBC connections from being closed.

src/main/kotlin/org/wfanet/measurement/common/db/liquibase/Liquibase.kt
@@ -35,31 +35,33 @@ import liquibase.resource.DirectoryResourceAccessor
 
 object Liquibase {
   fun update(connection: Connection, changelogPath: Path) {
-    val database =
-      DatabaseFactory.getInstance().findCorrectDatabaseImplementation(JdbcConnection(connection))
-    val resourceAccessor =
-      DirectoryResourceAccessor(changelogPath.fileSystem.rootDirectories.first())
-    val scopeObjects =
-      mapOf(
-        Scope.Attr.database.name to database,
-        Scope.Attr.resourceAccessor.name to resourceAccessor,
-      )
-
-    Scope.child(scopeObjects) {
-      Scope.getCurrentScope().setLogLevel(Level.INFO)
-
-      CommandScope(*UpdateCommandStep.COMMAND_NAME)
-        .apply {
-          addArgumentValue(DbUrlConnectionArgumentsCommandStep.DATABASE_ARG, database)
-          addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, changelogPath.toString())
-          addArgumentValue(
-            DatabaseChangelogCommandStep.CHANGELOG_PARAMETERS,
-            ChangeLogParameters(database),
+    DatabaseFactory.getInstance()
+      .findCorrectDatabaseImplementation(JdbcConnection(connection))
+      .use { database ->
+        val resourceAccessor =
+          DirectoryResourceAccessor(changelogPath.fileSystem.rootDirectories.first())
+        val scopeObjects =
+          mapOf(
+            Scope.Attr.database.name to database,
+            Scope.Attr.resourceAccessor.name to resourceAccessor,
           )
-          addArgumentValue(ShowSummaryArgument.SHOW_SUMMARY_OUTPUT, UpdateSummaryOutputEnum.LOG)
+
+        Scope.child(scopeObjects) {
+          Scope.getCurrentScope().setLogLevel(Level.INFO)
+
+          CommandScope(*UpdateCommandStep.COMMAND_NAME)
+            .apply {
+              addArgumentValue(DbUrlConnectionArgumentsCommandStep.DATABASE_ARG, database)
+              addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, changelogPath.toString())
+              addArgumentValue(
+                DatabaseChangelogCommandStep.CHANGELOG_PARAMETERS,
+                ChangeLogParameters(database),
+              )
+              addArgumentValue(ShowSummaryArgument.SHOW_SUMMARY_OUTPUT, UpdateSummaryOutputEnum.LOG)
+            }
+            .execute()
         }
-        .execute()
-    }
+      }
   }
 }
 