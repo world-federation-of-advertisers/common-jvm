beb08c2d9dc4b286394e31e79094f2628bb8de67
marcopremier <marcopremier@meta.com>
2024-11-07 03:39:40 -0800

    fix: add queueclient import to RabbitMqclient

--- src/main/kotlin/org/wfanet/measurement/common/rabbitmq/RabbitMqClient.kt
+++ src/main/kotlin/org/wfanet/measurement/common/rabbitmq/RabbitMqClient.kt
@@ -15,12 +15,10 @@
 package org.wfanet.measurement.common.rabbitmq
 
 import com.rabbitmq.client.AMQP
-import com.rabbitmq.client.Channel
 import com.rabbitmq.client.Connection
 import com.rabbitmq.client.ConnectionFactory
 import com.rabbitmq.client.DefaultConsumer
 import com.rabbitmq.client.Envelope
-import java.util.concurrent.ConcurrentHashMap
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
