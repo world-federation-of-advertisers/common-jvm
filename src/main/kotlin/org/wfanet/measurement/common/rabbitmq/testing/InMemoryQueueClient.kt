ef90eff07b96412d5680b6b576ebc7a747e368de
marcopremier <marcopremier@meta.com>
2024-11-07 08:32:04 -0800

    add: CoroutineContext to InMemoryQueueClient

--- src/main/kotlin/org/wfanet/measurement/common/rabbitmq/testing/InMemoryQueueClient.kt
+++ src/main/kotlin/org/wfanet/measurement/common/rabbitmq/testing/InMemoryQueueClient.kt
@@ -1,18 +1,23 @@
 package org.wfanet.measurement.common.rabbitmq.testing
 
+import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
 import org.wfanet.measurement.common.rabbitmq.QueueClient
 
 /** In-memory [QueueClient]. */
 @OptIn(ExperimentalCoroutinesApi::class) // For `produce`
-class InMemoryQueueClient : QueueClient {
-  private val scope = CoroutineScope(Dispatchers.Default)
-  private val messageChannel = Channel<ByteArray>(Channel.UNLIMITED)
+class InMemoryQueueClient(
+  blockingContext: CoroutineContext = Dispatchers.IO
+) : QueueClient {
+
+  private val scope = CoroutineScope(blockingContext)
+  private val messageChannel = Channel<ByteArray>()
 
   override fun <T> subscribe(queueName: String): ReceiveChannel<QueueClient.QueueMessage<T>> {
     return scope.produce {
@@ -28,6 +33,7 @@ class InMemoryQueueClient : QueueClient {
   }
 
   override fun close() {
+    scope.cancel()
     messageChannel.close()
   }
 
