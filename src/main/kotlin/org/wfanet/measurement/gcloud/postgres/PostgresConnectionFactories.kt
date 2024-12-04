60db72c519245469167d72bc9a4df60a2c560c77
Tristan Vuong <tristanvuong@google.com>
2024-12-04 19:46:53 +0000

    attempt connection pool

--- src/main/kotlin/org/wfanet/measurement/gcloud/postgres/PostgresConnectionFactories.kt
+++ src/main/kotlin/org/wfanet/measurement/gcloud/postgres/PostgresConnectionFactories.kt
@@ -34,7 +34,7 @@ object PostgresConnectionFactories {
         .option(ConnectionFactoryOptions.PASSWORD, "UNUSED")
         .option(ConnectionFactoryOptions.DATABASE, flags.database)
         .option(ConnectionFactoryOptions.HOST, flags.cloudSqlInstance)
-        .option(ConnectionFactoryOptions.STATEMENT_TIMEOUT, Duration.ofSeconds(30))
+        .option(ConnectionFactoryOptions.STATEMENT_TIMEOUT, Duration.ofSeconds(60))
         .option(GcpConnectionFactoryProvider.ENABLE_IAM_AUTH, true)
         .build()
     )
