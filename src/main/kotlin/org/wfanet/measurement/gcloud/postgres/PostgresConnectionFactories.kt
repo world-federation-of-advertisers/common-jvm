4c7056d3f56b64c94714289078605ef32018ac4e
Tristan Vuong <tristanvuong@google.com>
2024-12-06 03:37:56 +0000

    reduce max idle time

--- src/main/kotlin/org/wfanet/measurement/gcloud/postgres/PostgresConnectionFactories.kt
+++ src/main/kotlin/org/wfanet/measurement/gcloud/postgres/PostgresConnectionFactories.kt
@@ -41,7 +41,7 @@ object PostgresConnectionFactories {
 
     val configuration: ConnectionPoolConfiguration = ConnectionPoolConfiguration
       .builder(connectionFactory)
-      .maxIdleTime(Duration.ofMinutes(30))
+      .maxIdleTime(Duration.ofMinutes(10))
       .maxSize(20)
       .acquireRetry(30)
       .build()
