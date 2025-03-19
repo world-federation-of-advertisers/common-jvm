217944789cbf82d429489401a035777815434e75
Steven Ware Jones <stevenwarejones@gmail.com>
2025-03-19 19:27:37 -0400

    select StorageClient based on blob url (#296)

---
+++ src/main/kotlin/org/wfanet/measurement/storage/SelectedStorageClient.kt
@@ -0,0 +1,97 @@
+// Copyright 2025 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.storage
+
+import com.google.cloud.storage.StorageOptions
+import com.google.protobuf.ByteString
+import java.io.File
+import java.net.URI
+import kotlinx.coroutines.flow.Flow
+import org.wfanet.measurement.gcloud.gcs.GcsStorageClient
+import org.wfanet.measurement.storage.filesystem.FileSystemStorageClient
+
+// Data class to store parsed information
+data class BlobUri(val scheme: String, val bucket: String, val key: String)
+
+/*
+ * A [StorageClient] from a blob uri. Currently only supports Google Cloud Storage and File System.
+ * If file system storage should be supported, input a root File were data should be written/read.
+ * @param blobUri - the [BlobUri] from which a StorageClient can be built
+ * @param rootDirectory - only needed for file system storage clients
+ * @param projectId - optional if google cloud storage client needs require project id
+ */
+class SelectedStorageClient(
+  private val blobUri: BlobUri,
+  private val rootDirectory: File? = null,
+  private val projectId: String? = null,
+) : StorageClient {
+
+  constructor(
+    url: String,
+    rootDirectory: File? = null,
+    projectId: String? = null,
+  ) : this(parseBlobUri(url), rootDirectory, projectId)
+
+  val underlyingClient: StorageClient =
+    when (blobUri.scheme) {
+      "s3" -> {
+        throw IllegalArgumentException("S3 is not currently supported")
+      }
+      "gs" -> {
+        val storageOptions =
+          if (projectId != null) StorageOptions.newBuilder().setProjectId(projectId).build()
+          else StorageOptions.getDefaultInstance()
+        GcsStorageClient(storageOptions.service, requireNotNull(blobUri.bucket))
+      }
+      "file" -> {
+        FileSystemStorageClient(
+          checkNotNull(rootDirectory).toPath().resolve(blobUri.bucket).toFile()
+        )
+      }
+      else -> throw IllegalArgumentException("Unsupported blobUrl: $blobUri")
+    }
+
+  override suspend fun writeBlob(blobKey: String, content: Flow<ByteString>): StorageClient.Blob {
+    check(blobUri.key == blobKey)
+    return underlyingClient.writeBlob(blobKey, content)
+  }
+
+  override suspend fun getBlob(blobKey: String): StorageClient.Blob? {
+    check(blobUri.key == blobKey)
+    return underlyingClient.getBlob(blobKey)
+  }
+
+  companion object {
+
+    private fun parseBlobUri(url: String): BlobUri {
+      val uri = URI.create(url)
+      return when (uri.scheme) {
+        "s3" -> {
+          throw IllegalArgumentException("S3 is not currently supported")
+        }
+        "gs" -> {
+          val bucket = uri.host
+          val key = uri.path.removePrefix("/")
+          BlobUri(scheme = "gs", bucket = bucket, key = key)
+        }
+        "file" -> {
+          val (bucket, key) = uri.path.removePrefix("/").split("/", limit = 2)
+          BlobUri(scheme = "file", bucket = bucket, key = key)
+        }
+        else -> throw IllegalArgumentException("Unable to parse blob uri $url")
+      }
+    }
+  }
+}
---
+++ src/test/kotlin/org/wfanet/measurement/storage/SelectedStorageClientTest.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2025 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.storage
+
+import com.google.common.truth.Truth.assertThat
+import com.google.protobuf.kotlin.toByteStringUtf8
+import java.nio.file.Files
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertThrows
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.wfanet.measurement.common.flatten
+import org.wfanet.measurement.storage.filesystem.FileSystemStorageClient
+
+@RunWith(JUnit4::class)
+class SelectedStorageClientTest {
+  @Test
+  fun `s3 scheme throws IllegalArgumentException`() {
+    val s3Url = "s3://bucket.s3.us-west-2.amazonaws.com/path/to/file"
+    assertThrows(IllegalArgumentException::class.java) { SelectedStorageClient(s3Url) }
+  }
+
+  @Test
+  fun `gs scheme returns client`() {
+    val blobUri = BlobUri("gs", "bucket", "path/to/file")
+    SelectedStorageClient(blobUri)
+  }
+
+  @Test
+  fun `throws null if root directory is not present and scheme is file`() {
+    val blobUri = BlobUri("file", "bucket", "path/to/file")
+
+    assertThrows(IllegalStateException::class.java) { SelectedStorageClient(blobUri) }
+  }
+
+  @Test
+  fun `able to write blob from filesystem`() = runBlocking {
+    val fileUri = "file:///bucket/path/to/file"
+    val content = flowOf("a", "b", "c").map { it.toByteStringUtf8() }
+    val tmpPath = Files.createTempDirectory(null).toFile()
+    Files.createDirectories(tmpPath.resolve("bucket/path/to").toPath())
+    val client = SelectedStorageClient(fileUri, tmpPath)
+    client.writeBlob("path/to/file", content)
+    val fileSystemStorageClient = FileSystemStorageClient(tmpPath)
+    assertThat(fileSystemStorageClient.getBlob("bucket/path/to/file")!!.read().flatten())
+      .isEqualTo(content.flatten())
+  }
+
+  @Test
+  fun `able to write and read blob to filesystem`() = runBlocking {
+    val fileUri = "file:///bucket/path/to/file"
+    val content = flowOf("a", "b", "c").map { it.toByteStringUtf8() }
+    val tmpPath = Files.createTempDirectory(null).toFile()
+    Files.createDirectories(tmpPath.resolve("bucket/path/to").toPath())
+    val client = SelectedStorageClient(fileUri, tmpPath)
+    client.writeBlob("path/to/file", content)
+    val blob = client.getBlob("path/to/file")
+    assertThat(blob!!.read().flatten()).isEqualTo(content.flatten())
+  }
+
+  @Test
+  fun `read to invalid blob key throws error`() {
+    val fileUri = "file:///bucket/path/to/file"
+    val tmpPath = Files.createTempDirectory(null).toFile()
+    Files.createDirectories(tmpPath.resolve("bucket/path/to").toPath())
+    val client = SelectedStorageClient(fileUri, tmpPath)
+    assertThrows(IllegalStateException::class.java) {
+      runBlocking { client.getBlob("other-path/to/file") }
+    }
+  }
+
+  @Test
+  fun `write to invalid blob key throws error`() {
+    val fileUri = "file:///bucket/path/to/file"
+    val content = flowOf("a", "b", "c").map { it.toByteStringUtf8() }
+    val tmpPath = Files.createTempDirectory(null).toFile()
+    Files.createDirectories(tmpPath.resolve("bucket/path/to").toPath())
+    val client = SelectedStorageClient(fileUri, tmpPath)
+    assertThrows(IllegalStateException::class.java) {
+      runBlocking { client.writeBlob("other-path/to/file", content) }
+    }
+  }
+}
