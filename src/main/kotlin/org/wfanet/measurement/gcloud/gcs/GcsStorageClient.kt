c36188b0f98f8133a5ce6661de70852579e411db
Phi Hung Le <lephi@google.com>
2025-05-07 19:05:19 +0000

    Update function doc string.

--- src/main/kotlin/org/wfanet/measurement/gcloud/gcs/GcsStorageClient.kt
+++ src/main/kotlin/org/wfanet/measurement/gcloud/gcs/GcsStorageClient.kt
@@ -118,11 +118,11 @@ class GcsStorageClient(
 
   /** List file and folder names.
    *
-   * When prefix and delimiter options are not specified, all file names are returned.
-   * The prefix option filters out the file or folder names that do not match the prefix.
-   * The delimiter option (e.g. "/") are used with prefix to emulate hierarchy. When delimiter
-   * option is specified, the function returns full blob names for items that are directly under the
-   * current prefix and do not contain another instance of the delimiter after the prefix part.
+   * When prefix and delimiter options are not specified, all file names (blob keys) are returned.
+   * The prefix option filters out the file or folder names that do not match the prefix. The
+   * delimiter option (e.g. "/") are used with prefix to emulate hierarchy. When delimiter option is
+   * specified, the function returns full blob name for items that are directly under the current
+   * prefix and do not contain another instance of the delimiter after the prefix part.
    */
   suspend fun listBlobNames(
     prefix: String? = null,
