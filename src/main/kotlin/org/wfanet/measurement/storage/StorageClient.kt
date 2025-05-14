2b62417c59843b485d1cdc6242ed8b8c1e3bffc3
Tristan Vuong <tristanvuong@google.com>
2025-05-14 22:22:18 +0000

    lint fix

--- src/main/kotlin/org/wfanet/measurement/storage/StorageClient.kt
+++ src/main/kotlin/org/wfanet/measurement/storage/StorageClient.kt
@@ -50,11 +50,11 @@ interface StorageClient {
   /**
    * List file and folder names.
    *
-   * When prefix and delimiter options are not specified, all file names (blob keys) are returned. The
-   * prefix option filters out the file or folder names that do not match the prefix. The delimiter
-   * option (e.g. "/") are used with prefix to emulate hierarchy. When delimiter option is not
-   * specified, the function returns full blob name for items that are directly under the current prefix
-   * and do not contain another instance of the delimiter after the prefix part.
+   * When prefix and delimiter options are not specified, all file names (blob keys) are returned.
+   * The prefix option filters out the file or folder names that do not match the prefix. The
+   * delimiter option (e.g. "/") are used with prefix to emulate hierarchy. When delimiter option is
+   * not specified, the function returns full blob name for items that are directly under the
+   * current prefix and do not contain another instance of the delimiter after the prefix part.
    */
   suspend fun listBlobNames(prefix: String? = null, delimiter: String? = null): List<String>
 
