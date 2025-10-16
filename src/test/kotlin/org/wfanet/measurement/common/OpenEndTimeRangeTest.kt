world-federation-of-advertisers/common-jvm

commit 839c2cb4b833f1c070780a35f37258fae82862c1

feat: Add contains operator for time ranges (#349)
This also updates OpenEndTimeRange to implement OpenEndRange<Instant>

src/main/kotlin/org/wfanet/measurement/common/OpenEndTimeRange.kt
@@ -20,29 +20,21 @@ import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneOffset
 
-/**
- * Represents a range of [Instant]s where the upper bound is not included in the range.
- *
- * TODO(world-federation-of-advertisers/common-jvm#170): Implement OpenEndRange.
- */
-data class OpenEndTimeRange(val start: Instant, val endExclusive: Instant) {
-  operator fun contains(value: Instant): Boolean {
-    return value >= start && value < endExclusive
-  }
+/** Represents a range of [Instant]s where the upper bound is not included in the range. */
+class OpenEndTimeRange private constructor(private val delegate: OpenEndRange<Instant>) :
+  OpenEndRange<Instant> by delegate {
 
-  fun overlaps(other: OpenEndTimeRange): Boolean {
-    return max(start, other.start) < min(endExclusive, other.endExclusive)
-  }
+  constructor(start: Instant, endExclusive: Instant) : this(start..<endExclusive)
+
+  fun overlaps(other: OpenEndTimeRange): Boolean = overlaps(other as OpenEndRange<Instant>)
+
+  override fun equals(other: Any?): Boolean = delegate == other
+
+  override fun hashCode(): Int = delegate.hashCode()
+
+  override fun toString() = delegate.toString()
 
   companion object {
-    private fun min(lhs: Instant, rhs: Instant): Instant {
-      return if (lhs < rhs) lhs else rhs
-    }
-
-    private fun max(lhs: Instant, rhs: Instant): Instant {
-      return if (lhs > rhs) lhs else rhs
-    }
-
     fun fromClosedDateRange(
       dateRange: ClosedRange<LocalDate>,
       zoneOffset: ZoneOffset = ZoneOffset.UTC,
@@ -52,4 +44,12 @@ data class OpenEndTimeRange(val start: Instant, val endExclusive: Instant) {
         dateRange.endInclusive.plusDays(1).atStartOfDay().toInstant(zoneOffset),
       )
   }
+}
+
+fun OpenEndRange<Instant>.overlaps(other: OpenEndRange<Instant>): Boolean {
+  return start.coerceAtLeast(other.start) < endExclusive.coerceAtMost(other.endExclusive)
+}
+
+operator fun OpenEndRange<Instant>.contains(other: OpenEndRange<Instant>): Boolean {
+  return start <= other.start && endExclusive >= other.endExclusive
 }

commit b7d395fbf3d0fd3cee262448fc6986a2ad449d67

fix: Make OpenEndTimeRange.equals behave correctly (#352)
This was broken in #349 

Issue: world-federation-of-advertisers/cross-media-measurement#2998

src/main/kotlin/org/wfanet/measurement/common/OpenEndTimeRange.kt
@@ -21,28 +21,32 @@ import java.time.LocalDate
 import java.time.ZoneOffset
 
 /** Represents a range of [Instant]s where the upper bound is not included in the range. */
-class OpenEndTimeRange private constructor(private val delegate: OpenEndRange<Instant>) :
+@ConsistentCopyVisibility
+@Deprecated("Prefer OpenEndRange<Instant>")
+data class OpenEndTimeRange private constructor(private val delegate: OpenEndRange<Instant>) :
   OpenEndRange<Instant> by delegate {
 
+  @Deprecated("Prefer OpenEndRange<Instant>", ReplaceWith("start..<endExclusive"))
   constructor(start: Instant, endExclusive: Instant) : this(start..<endExclusive)
 
-  fun overlaps(other: OpenEndTimeRange): Boolean = overlaps(other as OpenEndRange<Instant>)
+  fun overlaps(@Suppress("DEPRECATION") other: OpenEndTimeRange): Boolean =
+    overlaps(other as OpenEndRange<Instant>)
 
-  override fun equals(other: Any?): Boolean = delegate == other
-
-  override fun hashCode(): Int = delegate.hashCode()
-
-  override fun toString() = delegate.toString()
+  override fun toString(): String = delegate.toString()
 
   companion object {
+    @Suppress("DEPRECATION")
+    @Deprecated(
+      "Use dateRange.toOpenEndInstantRange",
+      ReplaceWith(
+        "dateRange.toOpenEndInstantRange(zoneOffset)",
+        imports = ["org.wfanet.measurement.common.toOpenEndInstantRange"],
+      ),
+    )
     fun fromClosedDateRange(
       dateRange: ClosedRange<LocalDate>,
       zoneOffset: ZoneOffset = ZoneOffset.UTC,
-    ) =
-      OpenEndTimeRange(
-        dateRange.start.atStartOfDay().toInstant(zoneOffset),
-        dateRange.endInclusive.plusDays(1).atStartOfDay().toInstant(zoneOffset),
-      )
+    ) = OpenEndTimeRange(dateRange.toOpenEndInstantRange(zoneOffset))
   }
 }
 
@@ -52,4 +56,13 @@ fun OpenEndRange<Instant>.overlaps(other: OpenEndRange<Instant>): Boolean {
 
 operator fun OpenEndRange<Instant>.contains(other: OpenEndRange<Instant>): Boolean {
   return start <= other.start && endExclusive >= other.endExclusive
+}
+
+fun ClosedRange<LocalDate>.toOpenEndInstantRange(
+  zoneOffset: ZoneOffset = ZoneOffset.UTC
+): OpenEndRange<Instant> {
+  return start.atStartOfDay().toInstant(zoneOffset)..<endInclusive
+      .plusDays(1)
+      .atStartOfDay()
+      .toInstant(zoneOffset)
 }

src/test/kotlin/org/wfanet/measurement/common/OpenEndTimeRangeTest.kt
@@ -20,6 +20,7 @@ import com.google.common.truth.Truth.assertThat
 import java.time.Instant
 import java.time.LocalDate
 import java.time.Period
+import java.time.temporal.ChronoUnit
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -28,35 +29,61 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class OpenEndTimeRangeTest {
   @Test
-  fun `contains returns true for start`() {
-    val now = Instant.now()
-    val lastWeek = now.minus(Period.ofWeeks(1))
+  fun `equals returns true for equivalent instances`() {
+    val endExclusive = Instant.now()
+    val start = endExclusive.minus(5, ChronoUnit.MINUTES)
 
-    assertThat(lastWeek in OpenEndTimeRange(lastWeek, now)).isTrue()
+    @Suppress("DEPRECATION")
+    assertThat(OpenEndTimeRange(start, endExclusive))
+      .isEqualTo(OpenEndTimeRange(start, endExclusive))
   }
 
   @Test
-  fun `contains returns false for endExclusive`() {
-    val now = Instant.now()
-    val lastWeek = now.minus(Period.ofWeeks(1))
+  fun `equals returns false for inequivalent instances`() {
+    val endExclusive = Instant.now()
+    val start = endExclusive.minus(5, ChronoUnit.MINUTES)
 
-    assertThat(now in OpenEndTimeRange(lastWeek, now)).isFalse()
+    @Suppress("DEPRECATION")
+    assertThat(OpenEndTimeRange(start, endExclusive))
+      .isNotEqualTo(OpenEndTimeRange(start.minus(10, ChronoUnit.SECONDS), endExclusive))
   }
 
   @Test
-  fun `contains returns true for time between start and end`() {
+  fun `contains returns true for fully contained range`() {
     val now = Instant.now()
     val lastWeek = now.minus(Period.ofWeeks(1))
+    val range: OpenEndRange<Instant> = lastWeek..<now
+    val other = lastWeek.plus(1, ChronoUnit.SECONDS)..<now.minus(1, ChronoUnit.SECONDS)
 
-    assertThat(now.minusSeconds(1) in OpenEndTimeRange(lastWeek, now)).isTrue()
+    assertThat(other in range).isTrue()
+  }
+
+  @Test
+  fun `contains returns true for equivalent range`() {
+    val now = Instant.now()
+    val lastWeek = now.minus(Period.ofWeeks(1))
+    val range: OpenEndRange<Instant> = lastWeek..<now
+    val other: OpenEndRange<Instant> = lastWeek..<now
+
+    assertThat(other in range).isTrue()
+  }
+
+  @Test
+  fun `contains returns false non-contained range`() {
+    val now = Instant.now()
+    val lastWeek = now.minus(Period.ofWeeks(1))
+    val range: OpenEndRange<Instant> = lastWeek..<now
+    val other: OpenEndRange<Instant> = lastWeek.minus(1, ChronoUnit.SECONDS)..<now
+
+    assertThat(other in range).isFalse()
   }
 
   @Test
   fun `overlaps returns true for fully contained range`() {
     val now = Instant.now()
     val lastWeek = now.minus(Period.ofWeeks(1))
-    val subject = OpenEndTimeRange(lastWeek, now)
-    val other = OpenEndTimeRange(lastWeek.plus(Period.ofDays(1)), now.minus(Period.ofDays(1)))
+    val subject: OpenEndRange<Instant> = lastWeek..<now
+    val other: OpenEndRange<Instant> = lastWeek.plus(Period.ofDays(1))..<now.minus(Period.ofDays(1))
 
     assertThat(subject.overlaps(other)).isTrue()
   }
@@ -65,8 +92,9 @@ class OpenEndTimeRangeTest {
   fun `overlaps returns true for partially overlapping range`() {
     val now = Instant.now()
     val lastWeek = now.minus(Period.ofWeeks(1))
-    val subject = OpenEndTimeRange(lastWeek, now)
-    val other = OpenEndTimeRange(lastWeek.minus(Period.ofDays(1)), lastWeek.plus(Period.ofDays(1)))
+    val subject: OpenEndRange<Instant> = lastWeek..<now
+    val other: OpenEndRange<Instant> =
+      lastWeek.minus(Period.ofDays(1))..<lastWeek.plus(Period.ofDays(1))
 
     assertThat(subject.overlaps(other)).isTrue()
   }
@@ -75,18 +103,18 @@ class OpenEndTimeRangeTest {
   fun `overlaps returns false for range that only overlaps end`() {
     val now = Instant.now()
     val lastWeek = now.minus(Period.ofWeeks(1))
-    val subject = OpenEndTimeRange(lastWeek, now)
-    val other = OpenEndTimeRange(now, now.plus(Period.ofDays(1)))
+    val subject: OpenEndRange<Instant> = lastWeek..<now
+    val other: OpenEndRange<Instant> = now..<now.plus(Period.ofDays(1))
 
     assertThat(subject.overlaps(other)).isFalse()
   }
 
   @Test
-  fun `fromClosedDateRange returns range from start of first day to start of day after last day`() {
+  fun `toOpenEndInstantRange returns range from start of first day to start of day after last day`() {
     val firstDay = LocalDate.of(2023, 2, 1)
     val lastDay = LocalDate.of(2023, 2, 8)
 
-    val range = OpenEndTimeRange.fromClosedDateRange(firstDay..lastDay)
+    val range = (firstDay..lastDay).toOpenEndInstantRange()
 
     assertThat(range.start).isEqualTo(Instant.parse("2023-02-01T00:00:00Z"))
     assertThat(range.endExclusive).isEqualTo(Instant.parse("2023-02-09T00:00:00Z"))