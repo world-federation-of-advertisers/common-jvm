849eebc1f7f3857243af628a6b3f379c67de0768
marcopremier <marcopremier@meta.com>
2025-10-21 09:59:57 -0700

    add logs

--- src/main/kotlin/org/wfanet/measurement/common/crypto/tink/AeadStorageClient.kt
+++ src/main/kotlin/org/wfanet/measurement/common/crypto/tink/AeadStorageClient.kt
@@ -93,6 +93,7 @@ class AeadStorageClient(private val storageClient: StorageClient, private val ae
       val data = blob.read().flatten().toByteArray()
       println("-----> associated data: $associatedData / $blobKey")
       println("-----> data: $data")
+      println("----------> passing empty byte")
 //      emit(aead.decrypt(data, associatedData.toByteArray()).toByteString())
       emit(aead.decrypt(data, ByteArray(0)).toByteString())
     }
